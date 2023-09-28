1fb66a3e22585c18f8cb6519d1618071bdc3e976
harkais88 <63041960+harkais88@users.noreply.github.com>
2023-09-28 20:13:18 +0530

    Added solution bar to GUI and solution cells to notebooks

--- README.md
+++ README.md
@@ -1,10 +1,25 @@
 # python-practice
 
+<figure>
+    <blockquote>
+        Take up an idea, devote yourself, struggle in patience, and the sun will rise.<br>
+        <b><i>Swami Vivekananda</i></b>
+    </blockquote>
+</figure>
+
 This code generates a python test file consisting of questions taken from the files under the different topic directories. 10 questions are randomly chosen from each topic directory and are written to the test file.
 The test file can be either in the format of a text file (.txt) or a notebook file (.ipynb). 
 The option of which format is to be used is to be selected by the user using the GUI presented by the code.
 
+The text file format should be considered as practicing without a crutch. If you are not able to solve the 
+question at first, try to research more about relevant modules that can come to use. You can also google to see if there are any solutions provided in the online forums, although I would recommend to first try it by yourself until you are absolutely certain you cannot do it. Alternatively, you may use the search bar provided in the GUI to find a solution to the questions, although it is simply limited to the files that are provided in the topic directories.
+
+If you would like a more simpler or efficient practice approach, use the ipynb format. You would need a notebook viewer for this, like Jupyter Notebook or Google Collab Notebooks (at least this way, you don't have to install anything). A code space and kernel is provided this way, and while solutions are also provided along with the questions, they are hidden at first, and should only be opened after you try it yourself. 
+
 To use, download the testGen.py script and atleast one of the different topic directories and put them in the same directory. Then, simply run the script and the GUI will be presented. Aternatively, you may want to convert the script into an .exe file using pyinstaller, for which I would suggest using a spec file for building exe.
+
+If you simply want to execute the testGen.py script, the following modules are needed to be installed using pip:
+nbformat, tkinter, ttkwidgets, art(This one is actually optional, if you don't like this, open up the script, go to the txtOps function, and find the OPTIONAL comment)
 
 All of these questions are taken from the W3Resource Website
 https://www.w3resource.com/python-exercises/
--- testGen.py
+++ testGen.py
@@ -6,19 +6,23 @@ import os,random
 from art import text2art
 import nbformat as nbf
 import tkinter as tk
+from ttkwidgets.autocomplete import AutocompleteCombobox
 import webbrowser
 
 class testGen:
     """ Class for generating tests based on the practice files\n
     Use txtOps() for txt file format or nbOps() for ipynb format """
 
-    curr_dir = os.path.dirname(os.path.realpath(__file__))
-    test_dir = "Test Attempts"
+    def __init__(self):
+        self.curr_dir = os.path.dirname(os.path.realpath(__file__))
+        self.test_dir = "Test Attempts"
+        self.file_paths_list = self.getFilesPathsList()
 
     def getTopicDirs(self):
         """ Get the list of all the different topic directories"""
 
-        return [dir for dir in os.listdir(self.curr_dir) if (os.path.isdir(dir) and dir != self.test_dir and dir[0] != ".")]
+        return [dir for dir in os.listdir(self.curr_dir) 
+                if (os.path.isdir(dir) and dir != self.test_dir and dir[0] != ".")]
 
     def checkTestDir(self):
         """  The test files go into this directory, if it does not exist, we create it"""
@@ -54,10 +58,12 @@ class testGen:
         self.checkTestDir()
 
         # Setting Test File Number, as there would be multiple test files 
-        file_no = len([file for file in os.listdir(f"{self.curr_dir}/{self.test_dir}") if file.endswith(".txt")]) + 1
+        file_no = len([file for file in os.listdir(f"{self.curr_dir}/{self.test_dir}") 
+                       if file.endswith(".txt")]) + 1
 
         # Setting Test File Path
-        self.test_file_path = os.path.abspath(os.path.normpath(f"{self.curr_dir}/{self.test_dir}/Python_Test_{file_no}.txt"))
+        self.test_file_path = os.path.abspath(os.path.normpath(
+            f"{self.curr_dir}/{self.test_dir}/Python_Test_{file_no}.txt"))
 
         # Creating our test file
         test_file = open(self.test_file_path,"w")
@@ -66,14 +72,19 @@ class testGen:
         test_file.write(text2art("Python Test".center(85), font = "ogre"))
 
         # Iterating through each list of relative path names of the different topic dirs inside
-        # the file_paths_list list 
-        for file_paths in self.getFilesPathsList():
+        # the file_paths_list list
+        self.getFilesPathsList()
+
+        for file_paths in self.file_paths_list:
             # To track the files that have been read, and to set it to only read from a set number of files
             file_log = []
 
             # Writing the topic name as the sub-Header, to distinguish between the different topics...
             # Maybe this won't be required, and in the future, should look into jumbling the order up too
-            test_file.write(text2art(file_paths[0][:file_paths[0].find(os.path.basename(file_paths[0]))-1], font = "ogre") + "\n")
+            test_file.write(text2art(file_paths[0][:file_paths[0].find(os.path.basename(file_paths[0]))-1], 
+                                     font = "ogre") + "\n")
+            # OPTIONAL: IF DECORATED SUB HEADERS ARE NOT REQUIRED, REMOVE COMMENT FROM NEXT LINE
+            #test_file.write(file_paths[0][:file_paths[0].find(os.path.basename(file_paths[0]))-1])
 
             # Writing our 10 questions from the current topic
             while len(file_log) != 10:
@@ -130,7 +141,9 @@ class testGen:
 
         # Iterating through each list of relative path names of the different topic dirs inside
         # the file_paths_list list 
-        for file_paths in self.getFilesPathsList():
+        self.getFilesPathsList()
+
+        for file_paths in self.file_paths_list:
             # To track the files that have been read, and to set it to only read from a set number of files
             file_log = []
 
@@ -147,7 +160,6 @@ class testGen:
             #             replace("_","&lowbar;").replace(" ","&#160;").replace("\n","<br>").replace("\\","&bsol;")
             #                                             + "\n"))
             
-
             # Writing our 10 questions from the current topic
             while len(file_log) != 10:
 
@@ -161,33 +173,41 @@ class testGen:
                 file = open(file_path, "r")
                 lines = file.readlines()
 
-                # Setting our result variable
+                # Setting our question and solution result variable
                 # Should contain the entire question
-                r_line = ""
+                q_line = ""
+                # Should contain the entire solution
+                s_line = ""
 
-                # Iterating through each line in the file
-                for line in lines:
-
-                    # Removing any """ found
-                    # Replacing \n with <br> and \t with &#160; 4 times for HTML formatting
+                index = 0
+                line = lines[index]
+                while True:
                     f_line = line.replace("\"\"\"","").replace("\n","<br>").replace("\t","&#160;"*4)
-                    # If needed, you can set it to the exact format given in the files
-                    # w_line = "<pre>"+w_line+"</pre>"
-
-                    # Adding our formatted line to the result variable
-                    r_line += f_line
-
-                    # If """\n exists in the line, it means we reached the end of the multiline comment
-                    # It also coincidentally means we reached the end of our question too
+                    q_line += f_line
                     if "\"\"\"\n" in line:
                         break
-                
+                    index += 1
+                    line = lines[index]
+
+                for i in range(index+1,len(lines)):
+                    line = lines[i]
+                    f_line = line.replace("\"\"\"","").replace("\n","<br>")
+                    firstCharIndex = next(filter(str.isalpha, f_line), None)
+                    if firstCharIndex is not None:
+                        f_line = f_line.replace(" ", "&#160;", f_line.find(firstCharIndex))
+                    s_line += f_line
+
                 # Creating a new markdown cell with the result variable, thus writing a new question
                 nb["cells"].append(nbf.v4.new_markdown_cell(str(len(file_log)+1) + ". "+ 
-                                                            r_line))
+                                                            q_line))
                 # Creating a code space for the user to test their code on
                 nb["cells"].append(nbf.v4.new_code_cell(code_lines))
-
+                # Creating a markdown cell for the solution
+                nb["cells"].append(nbf.v4.new_markdown_cell(
+                                        "<details><summary style = \"font-size: 18px;\">Show Solution</summary>"+
+                                        "<span style=\"font-size: 15px; color: #03A062;\">" 
+                                        + s_line + "</span>"))
+                
                 # Closing the file we took the question from
                 file.close()
                 # Logging the file to ensure uniqueness
@@ -209,14 +229,43 @@ class testGen:
         test_file.close()
 
 class GUI:
+    testObj = testGen()
+
     def __init__(self):
         """ Presents a GUI for the user to decide whether they want\n
         to make the test file a txt file or an ipynb file"""
+
+        # Get Question list for solution finding and autocomplete feature
+        self.getQuestions()
 
         self.gui = tk.Tk()
         self.gui.title("Python Test Generator")
         self.draw()
         self.gui.mainloop()
+
+    def getQuestions(self):
+        self.questions_list = []
+        r_line = ""
+
+        for file_paths in self.testObj.file_paths_list:
+            for file_path in file_paths:
+                with open(file_path,"r") as file:
+                    lines = file.readlines() 
+
+                    # The questions are written in the first multiline comment in each file
+                    for line in lines:
+                        # Removing all instances of the comment quotations
+                        w_line = line.replace("\"\"\"","")
+
+                        # Writing our line to the test file
+                        r_line += w_line
+
+                        # If the string """"\n" is encountered, it means the end of the comment
+                        if "\"\"\"\n" in line:
+                            break
+
+                self.questions_list.append(r_line)
+                r_line = ""    
 
     def draw(self):
         """ Responsible for drawing GUI Elements"""
@@ -244,25 +293,91 @@ class GUI:
         self.output = tk.Label(self.gui, text = "")
         self.output.grid(row = 3, column= 1)
 
+        # Search bar
+        search_label_head = tk.Label(self.gui,
+                                     anchor="w",
+                                     text = "Use this search bar for accessing solution files\n"+
+                                     " Not required for ipynb files",
+                                     font = ("Helvetica", 10, "bold"))
+        search_label_head.grid(row = 4, column=1, pady = 10)
+
+        self.search_frame = tk.Frame(self.gui)
+        self.search_frame.grid(row = 6, column = 1, pady = 10)
+
+        search_label = tk.Label(self.search_frame, text = "Enter Question Here", 
+                                font = ("Helvetica", 8, "bold"))
+        search_label.pack(side = tk.LEFT)
+
+        self.search_bar = AutocompleteCombobox(self.search_frame, 
+                                                   completevalues = self.questions_list, 
+                                                   width = 80)
+        self.search_bar.pack(side = tk.LEFT, padx = 10)
+
+        self.search_get = tk.Button(self.search_frame, text = "GET SOLUTION", command = self.showSol)
+        self.search_get.pack(side = tk.LEFT)
+
+        self.search_output = tk.Label(self.gui, text = "")
+        self.search_output.grid(row = 8, column = 1, pady = 10)
+
     def onTxt(self):
         """ On clicking this button, user decided to make it a txt file"""
 
-        testObj = testGen()
-        testObj.txtOps()
-        test_file_name = testObj.test_file_path[testObj.test_file_path.find("Python_Test"):]
+        self.testObj.txtOps()
+        test_file_name = self.testObj.test_file_path[self.testObj.test_file_path.find("Python_Test"):]
         self.output.configure(text = f"{test_file_name} created! Click on me to check me out",
                               fg = "blue", font = ("Helvetica",12))
-        self.output.bind("<Button-1>", lambda e: webbrowser.open_new(testObj.test_file_path))
+        self.output.bind("<Button-1>", lambda e: webbrowser.open_new(self.testObj.test_file_path))
 
     def onIpynb(self):
         """ On clicking this button, user decided to make it a ipynb file"""
 
-        testObj = testGen()
-        testObj.nbOps()
-        test_file_name = testObj.test_file_path[testObj.test_file_path.find("Python_Test"):]
+        self.testObj.nbOps()
+        test_file_name = self.testObj.test_file_path[self.testObj.test_file_path.find("Python_Test"):]
         self.output.configure(text = f"{test_file_name} created! Click on me to check me out",
                               fg = "blue", font = ("Helvetica",12))
-        self.output.bind("<Button-1>", lambda e: webbrowser.open_new(testObj.test_file_path))
+        self.output.bind("<Button-1>", lambda e: webbrowser.open_new(self.testObj.test_file_path))
+
+    def showSol(self):
+        search_bar_output = self.search_bar.get()
+        if search_bar_output[0].isdigit():
+            search_bar_output = search_bar_output[search_bar_output.find(
+                next(filter(str.isalpha, search_bar_output))):]
+        if all(search_bar_output not in question for question in self.questions_list):
+            self.search_output.configure(text = "Enter the correct question", fg="red")
+        else:
+            r_line = ""
+            r_file_path = ""
+
+            for file_paths in self.testObj.file_paths_list:
+                flag = False
+                for file_path in file_paths:
+                    with open(file_path,"r") as file:
+                        lines = file.readlines() 
+
+                        # The questions are written in the first multiline comment in each file
+                        for line in lines:
+                            # Removing all instances of the comment quotations
+                            w_line = line.replace("\"\"\"","")
+
+                            # Writing our line to the test file
+                            r_line += w_line
+
+                            # If the string """"\n" is encountered, it means the end of the comment
+                            if "\"\"\"\n" in line:
+                                break
+                    
+                    if search_bar_output in r_line:
+                        r_file_path = file_path
+                        flag = True
+                        break
+                    else:
+                        r_line = ""
+                if flag:
+                    break
+
+            if r_line != "":
+                self.search_output.configure(text = "Solution found! Click me to open the file", fg="blue")
+                self.search_output.bind("<Button-1>", lambda e: webbrowser.open_new(r_file_path))
 
 if __name__ == "__main__":
     GUI()
